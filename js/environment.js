ea63e8ad1d10a077dfff1f99998f4a900895952b
Ray <lyraymond53@berkeley.edu>
2021-02-19 17:34:15 -0800

    add scene lighting, plane warp effect

--- js/environment.js
+++ js/environment.js
@@ -2,7 +2,7 @@ var Colors = {
     black: 0x14051C,
     purple: 0x351245,
     lightPurple: 0xB373D1,
-    lightBlue: 0x83CBE6
+	lightBlue: 0x68c3c0
 };
 
 window.addEventListener('load', init, false);
@@ -10,8 +10,9 @@ window.addEventListener('load', init, false);
 function init() {
 	// Initialize the scene, the camera and the renderer
 	createScene();
-	createPoint();
-	createSea();
+
+	createPlane();
+	createLights();
 
 	animate();
 }
@@ -46,7 +47,7 @@ function createScene() {
 
 	// Set the position of the camera
 	camera.position.x = 0;
-	camera.position.z = 200;
+	camera.position.z = 1000;
 	camera.position.y = 0;
 
 	// Create the renderer
@@ -62,7 +63,7 @@ function createScene() {
 	renderer.setSize(WIDTH, HEIGHT);
 
 	// Enable shadow rendering
-	//renderer.shadowMap.enabled = true;
+	renderer.shadowMap.enabled = true;
 
 	// Add the DOM element of the renderer to the container we created in the HTML
 	container = document.getElementById('space');
@@ -81,43 +82,27 @@ function handleWindowResize() {
 	camera.updateProjectionMatrix();
 }
 
-function createPoint() {
-	var pointGeometry = new THREE.SphereGeometry(2, 16, 16);
-	var pointMaterial = new THREE.MeshBasicMaterial({ color: 0xffff00 });
-	var point3D = new THREE.Mesh(pointGeometry, pointMaterial);
-
-	scene.add(point3D);
+function createPlane() {
+	plane = new Plane();
+	scene.add(plane.mesh);
 }
 
-function createSea() {
-	sea = new Sea();
+Plane = function () {
+	var PlaneGeom = new THREE.PlaneGeometry(1200, 800, 10, 10);
 
-	// push it a little bit at the bottom of the scene
-	sea.mesh.position.y = -600;
+	// Keep the mesh continuous	
+	PlaneGeom.mergeVertices();
 
-	// add the mesh of the sea to the scene
-	scene.add(sea.mesh);
-}
+	// Get number of vertices
+	var vertices = PlaneGeom.vertices.length;
 
-
-Sea = function () {
-	var geom = new THREE.CylinderGeometry(600, 600, 800, 40, 10);
-	geom.applyMatrix(new THREE.Matrix4().makeRotationX(-Math.PI / 2));
-
-	// important: by merging vertices we ensure the continuity of the waves
-	geom.mergeVertices();
-
-	// get the vertices
-	var l = geom.vertices.length;
-
-	// create an array to store new data associated to each vertex
+	// Make an array to store new vertex data
 	this.waves = [];
 
-	for (var i = 0; i < l; i++) {
-		// get each vertex
-		var v = geom.vertices[i];
+	// Iterate through and modify each vertex
+	for (var i = 0; i < vertices; i++) {
+		var v = PlaneGeom.vertices[i];
 
-		// store some data associated to it
 		this.waves.push({
 			y: v.y,
 			x: v.x,
@@ -127,24 +112,81 @@ Sea = function () {
 			// a random distance
 			amp: 5 + Math.random() * 15,
 			// a random speed between 0.016 and 0.048 radians / frame
-			speed: 0.016 + Math.random() * 0.032
+			speed: 0.016 + Math.random()/2 * 0.032
 		});
 	};
-	var mat = new THREE.MeshPhongMaterial({
-		color: Colors.blue,
+
+	// Skew the plane
+	PlaneGeom.applyMatrix(new THREE.Matrix4().makeRotationX(-Math.PI / 9));
+	//PlaneGeom.applyMatrix(new THREE.Matrix4().makeRotationY(Math.PI / 6));
+	//PlaneGeom.applyMatrix(new THREE.Matrix4().makeRotationZ());
+
+	var PlaneMat = new THREE.MeshPhongMaterial({
+		color: Colors.lightBlue,
 		transparent: true,
-		opacity: .8,
-		shading: THREE.FlatShading,
+		opacity: 1,
+		shading: THREE.FlatShading
 	});
 
-	this.mesh = new THREE.Mesh(geom, mat);
+	this.mesh = new THREE.Mesh(PlaneGeom, PlaneMat);
+	this.mesh.castShadow = true;
 	this.mesh.receiveShadow = true;
 
 }
 
-function animate() {
-	sea.mesh.rotation.z += .005;
+Plane.prototype.planeWave = function () {
+	var vertices = this.mesh.geometry.vertices;
+	var numVertices = vertices.length;
 
+	for (var i = 0; i < numVertices; i++) {
+		var v = vertices[i];
+		var planeV = this.waves[i];
+
+		// update the position of the vertex
+		v.x = planeV.x + Math.cos(-planeV.ang) * planeV.amp;
+		v.y = planeV.y + Math.sin(-planeV.ang) * planeV.amp;
+
+		// increment the angle for the next frame
+		planeV.ang += planeV.speed;
+	}
+	this.mesh.geometry.verticesNeedUpdate = true;
+}
+
+var hemiLight, directionLight;
+
+function createLights() {
+
+
+	directionLight = new THREE.DirectionalLight(0xffffff, 1);
+	//hemiLight = new THREE.HemisphereLight(0xffffff, 0xffffff, .9);
+
+	directionLight.position.set(50, 120, 150);
+
+	directionLight.castShadow = true;
+
+	// Shadow projection boundaries
+	directionLight.shadow.camera.left = -400;
+	directionLight.shadow.camera.right = 400;
+	directionLight.shadow.camera.top = 400;
+	directionLight.shadow.camera.bottom = -400;
+
+
+	directionLight.shadow.camera.near = 1;
+	directionLight.shadow.camera.far = 1000;
+
+	// Shadow resolution
+	directionLight.shadow.mapSize.width = 2048;
+	directionLight.shadow.mapSize.height = 2048;
+
+	scene.add(directionLight);
+	//scene.add(hemiLight);
+}
+
+
+
+
+function animate() {
+	plane.planeWave();
 	renderer.render(scene, camera);
 	requestAnimationFrame(animate);
 }
